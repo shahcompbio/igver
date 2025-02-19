df613634a32ddce1d3b691e0aa27cd795106221c
Seongmin Choi <soymintc@gmail.com>
2025-02-19 17:23:29 -0500

    fix path problems

--- igver/igver.py
+++ igver/igver.py
@@ -1,5 +1,5 @@
-import io
 import os
+import sys
 import subprocess
 import uuid
 import time
@@ -8,12 +8,12 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 
-def load_screenshot(bam_paths, regions, output_dir="/tmp", genome="hg19", igv_dir="/opt/IGV_2.14.1", 
+def load_screenshot(bam_paths, regions, output_dir='/tmp', genome="hg19", igv_dir="/opt/IGV_2.14.1", 
                     overwrite=True, remove_png=True, dpi=300,
                     singularity_image='docker://quay.io/soymintc/igver', singularity_args='-B /home',
                     debug=False, **kwargs):
     """
-    Generates IGV screenshots and loads them into Matplotlib figures.
+    Generates IGV screenshots and loads them into a Matplotlib figure.
 
     Parameters:
         bam_paths (list of str): Paths to BAM files.
@@ -23,62 +23,64 @@ def load_screenshot(bam_paths, regions, output_dir="/tmp", genome="hg19", igv_di
         igv_dir (str, optional): Directory containing IGV installation (default: "/opt/IGV_2.14.1").
         overwrite (bool, optional): Whether to overwrite existing PNG files (default: True).
         remove_png (bool, optional): Whether to remove created PNG files (default: True).
-        dpi (int, optional): DPI of the figures (default: 300).
+        dpi (int, optional): DPI of the figure (default: 300).
         singularity_image (str, optional): singularity image path (default: "docker://shahcompbio/igver").
         singularity_args (str, optional): singularity arguments string (default: "-B /home").
         debug (bool, optional): Whether to show logs for debugging (default: False).
         **kwargs (optional): *kwargs* such as tag, max_panel_height, overlap_display, igv_config for create_batch_script
 
     Returns:
-        list of matplotlib.figure.Figure: List of Matplotlib figures containing the IGV screenshots.
+        matplotlib.figure.Figure: Matplotlib figure containing the IGV screenshots.
     """
     from .igver import create_batch_script, run_igv  # Import helper functions
 
     # Create batch script and expected PNG paths
     tmpdir = os.getenv("TMPDIR", output_dir)  # Default to /tmp if TMPDIR is not set
+    if output_dir == '/tmp':
+        output_dir = os.environ['TMPDIR']
     if debug:
         print(f"[LOG:{time.ctime()}] TMPDIR is set to: {tmpdir}")
-    batch_script, png_paths = create_batch_script(bam_paths, regions, output_dir, genome)
-    bam_dirs = set([os.path.split(p)[0] for p in bam_paths])
-    for bam_dir in bam_dirs:
-        singularity_args += f' -B {os.path.abspath(bam_dir)}'
-    singularity_args += f' -B {os.path.abspath(output_dir)}'
-    if tmpdir != output_dir:
-        singularity_args += f' -B {tmpdir}'
+    batch_script, png_path = create_batch_script(bam_paths, regions, output_dir, genome, **kwargs)
+    for bam_path in bam_paths:
+        abspath = os.path.abspath(bam_path)
+        realpath = os.path.realpath(bam_path)
+        bam_dir_abs = os.path.split(abspath)[0]
+        bam_dir_real = os.path.split(realpath)[0]
+        singularity_args += f' -B {bam_dir_abs}'
+        if bam_dir_abs != bam_dir_real:
+            singularity_args += f' -B {bam_dir_real}'
+    singularity_args += f' -B {os.path.realpath(output_dir)}'
+    singularity_args += f' -B {tmpdir}'
 
     # Run IGV to generate the screenshots
-    run_igv(batch_script, png_paths, igv_dir, overwrite, 
+    run_igv(batch_script, png_path, igv_dir, overwrite, 
         singularity_image=singularity_image, singularity_args=singularity_args, debug=debug)
 
     # Check if screenshots were generated
-    if not png_paths:
+    if not png_path:
         raise RuntimeError("[ERROR] No screenshots generated.")
 
     # Load each screenshot into a Matplotlib figure with high resolution
-    figures = []
-    for png_path in png_paths:
-        image = Image.open(png_path)
-        width, height = image.size  # Get original image dimensions
+    image = Image.open(png_path)
+    width, height = image.size  # Get original image dimensions
 
-        # Convert to inches for Matplotlib
-        figsize = (width / dpi, height / dpi)
+    # Convert to inches for Matplotlib
+    figsize = (width / dpi, height / dpi)
 
-        fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
-        ax.imshow(image)
-        ax.axis("off")
+    fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
+    ax.imshow(image)
+    ax.axis("off")
 
-        figures.append(fig)
+    # Remove the temp PNG if requested
+    if remove_png:
+        os.remove(png_path)
+        if debug:
+            print(f"[LOG:{time.ctime()}] Removed image {png_path}")
 
-        # Remove the temp PNG if requested
-        if remove_png:
-            os.remove(png_path)
-            if debug:
-                print(f"[LOG:{time.ctime()}] Removed image {png_path}")
-
-    return figures
+    return fig
 
 
-def run_igv(batch_script, png_paths, igv_dir="/opt/IGV_2.14.1", overwrite=False, 
+def run_igv(batch_script, png_path, igv_dir="/opt/IGV_2.14.1", overwrite=False, 
             singularity_image='docker://shahcompbio/igver', singularity_args='-B /data1 -B /home',
             debug=False):
     """
@@ -86,7 +88,7 @@ def run_igv(batch_script, png_paths, igv_dir="/opt/IGV_2.14.1", overwrite=False,
 
     Parameters:
         batch_script (str): Path to the IGV batch script.
-        png_paths (list of str): Expected paths of the output PNG screenshots.
+        png_path (str): Expected path of the output PNG screenshot.
         igv_dir (str, optional): Directory containing IGV installation (default: "/opt/IGV_2.14.1").
         overwrite (bool, optional): Whether to overwrite existing PNG files (default: False).
         debug (bool, optional): Whether to show logs for debugging (default: False).
@@ -106,22 +108,25 @@ def run_igv(batch_script, png_paths, igv_dir="/opt/IGV_2.14.1", overwrite=False,
 
     # If overwrite is enabled, remove existing PNG files
     if overwrite:
-        for png_path in png_paths:
-            if os.path.exists(png_path):
-                os.remove(png_path)
-                if debug:
-                    print(f"[LOG:{time.ctime()}] Removed existing {png_path}")
+        if os.path.exists(png_path):
+            os.remove(png_path)
+            if debug:
+                print(f"[LOG:{time.ctime()}] Removed existing {png_path}")
 
     # Run IGV
     n_iter = 0
     max_iter = 10
-    while not all(os.path.exists(png) for png in png_paths) and n_iter < max_iter:
+    while not os.path.exists(png_path) and n_iter < max_iter:
         if debug:
             print(f"[LOG:{time.ctime()}] Iteration #{n_iter + 1}: Ensuring PNG files exist")
-        subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        # Print STDOUT and STDERR if debug=True
+        if debug:
+            print(f"[STDOUT:{time.ctime()}]\n{result.stdout}")
+            print(f"[STDERR:{time.ctime()}]\n{result.stderr}")
         n_iter += 1
 
-    if not all(os.path.exists(png) for png in png_paths):
+    if not os.path.exists(png_path):
         raise RuntimeError(f"[ERROR:{time.ctime()}] Failed to generate all PNG files after {max_iter} iterations.")
 
     # Cleanup batch script
@@ -129,7 +134,7 @@ def run_igv(batch_script, png_paths, igv_dir="/opt/IGV_2.14.1", overwrite=False,
     if debug:
         print(f"[LOG:{time.ctime()}] Removed batch script {batch_script}")
 
-    return png_paths
+    return png_path
 
 
 def create_batch_script(bam_paths, regions, output_dir, genome='hg19', tag='tumor', max_panel_height=200,
@@ -174,20 +179,21 @@ def create_batch_script(bam_paths, regions, output_dir, genome='hg19', tag='tumo
     for bam in bam_paths:
         batch_content.append(f'load {bam}')
     
-    png_paths = []
+    region_tags = []
+    merged_region = ' '.join(regions)
+    batch_content.append(f'goto {merged_region}')
     for region in regions:
         region_tag = region.replace(':', '-')
-        png_fname = f"{region_tag}.{tag}.png"
-        png_path = os.path.join(output_dir, png_fname)
-        png_paths.append(png_path)
-        
-        batch_content.append(f'goto {region}')
-        if overlap_display != 'expand':
-            batch_content.append(overlap_display)
-        batch_content.append(f'maxPanelHeight {max_panel_height}')
-        if additional_pref:
-            batch_content.append(additional_pref)
-        batch_content.append(f'snapshot {png_fname}')
+        region_tags.append(region_tag)
+    png_fname = '.'.join(region_tags) + '.' + tag + '.png'
+    png_path = os.path.join(output_dir, png_fname)
+    
+    if overlap_display != 'expand':
+        batch_content.append(overlap_display)
+    batch_content.append(f'maxPanelHeight {max_panel_height}')
+    if additional_pref:
+        batch_content.append(additional_pref)
+    batch_content.append(f'snapshot {png_fname}')
     
     batch_content.append('exit')
     
@@ -195,5 +201,5 @@ def create_batch_script(bam_paths, regions, output_dir, genome='hg19', tag='tumo
     with open(batch_filename, 'w') as batch_file:
         batch_file.write('\n'.join(batch_content) + '\n')
     
-    return batch_filename, png_paths
+    return batch_filename, png_path
 
