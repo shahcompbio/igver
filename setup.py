80dee8de3af38d9033a4662660842fd2be62e54d
Seongmin Choi <soymintc@gmail.com>
2025-03-18 15:46:56 -0400

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='igver',
-    version='0.8',
+    version='0.9',
     packages=find_packages(),
     package_data={
         'igver': ['data/genome_map.yaml'],
